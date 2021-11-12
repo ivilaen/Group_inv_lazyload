3de13a92b80042eb737303a6252ca7e4a31986d6
Rishav Kumar <59506527+ivilaen@users.noreply.github.com>
2021-11-12 19:00:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,493 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+  <link rel="stylesheet" href="./css/styles.css" />
+  <title>GroupInvest - Investment opportunity</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <header id="home" class="header">
+     <!-- Navigation -->
+  <nav class="nav">
+    <div class="navigation container">
+      <div class="logo">
+        <h1>GroupInvest</h1>
+      </div>
+
+      <div class="menu">
+        <div class="top-nav">
+          <div class="logo">
+            <h1>GroupInvest</h1>
+          </div>
+          <div class="close">
+            <i class="bx bx-x"></i>
+          </div>
+        </div>
+
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#hoi" class="nav-link">How to Invest</a>
+          </li>
+          <li class="nav-item">
+            <a href="#assets" class="nav-link">Assets</a>
+          </li>
+          <li class="nav-item">
+            <a href="#our_partner" class="nav-link">Our Partners</a>
+          </li>
+          <li class="nav-item">
+            <a href="acc.php" class="nav-link">Login/Register</a>
+          </li>
+        </ul>
+      </div>
+      <div class="hamburger">
+        <i class="bx bx-menu"></i>
+      </div>
+    </div>
+  </nav>
+
+    <img src="./images/banner.png" alt="" class="hero-img" />
+
+    <div class="hero-content">
+      <h2><span class="discount"></span> </span> Invest in physical assets such as vehicles, equipment and furniture,<br> leased to corporates, to earn great returns</h2>
+      <h1>
+        <span>Small Investment,</span>
+        <span>Big Returns</span>
+      </h1>
+      <a class="btn" href="#">Invest Now</a>
+    </div>
+  </header>
+
+  <!-- Main -->
+  <main>
+    <section class="advert section">
+      <div class="advert-center container">
+        <div class="advert-box">
+          <div class="dotted">
+            <div class="content">
+              <h2>
+                Diversify your portfolio starting<br>
+                at Rs 20,000
+              </h2>
+              <h4>Start your Investment</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="advert-box">
+          <div class="dotted">
+            <div class="content">
+              <h2>
+                Attractive rate of returns
+              </h2>
+              <h4>Start your Investment</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="advert-box">
+          <div class="dotted">
+            <div class="content">
+              <h2>
+                Fixed, non market linked returns<br>  
+                with monthly payouts
+              </h2>
+              <h4>Start your Investment</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+     
+
+    <section class="section featured">
+      <div class="title">
+        <h1>Recent Deals</h1>
+      </div>
+
+      <div class="product-center container">
+        <div class="product">
+          <div class="product-header">
+            <img class="lazy" data-src="./images/pic2.png" />
+          </div>
+          <div class="product-footer">
+            <a href="#">
+              <h3>StanPlus Powered Ambulance</h3>
+            </a>
+            
+            <h4 class="price">Investment raised: 1000cr<br>
+              IRR: 21<br>
+              Investors: 90<br>
+              On Time Payment: 87%<br>
+            </h4>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-header">
+            <img class="lazy" data-src="./images/pic1.png" />
+          </div>
+          <div class="product-footer">
+            <a href="#"><h3>VOGO</h3></a>
+            
+            <h4 class="price">Investment raised: 0.85cr<br>
+              IRR: 96<br>
+              Investors: 43<br>
+              On Time Payment: 90%<br>
+          </h4>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-header">
+            <img class="lazy" data-src="./images/pic3.png" />
+          </div>
+          <div class="product-footer">
+            <a href="#"><h3>ZYPP</h3></a>
+            <h4 class="price">
+              Investment raised: 2.45cr<br>
+              IRR: 26<br>
+              Investors: 213<br>
+              On Time Payment: 100%<br>
+
+            </h4>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-header">
+            <img class="lazy" data-src="./images/pic4.png" />
+          </div>
+          <div class="product-footer">
+            <a href="#"><h3>IPL Tech Electric</h3></a>
+            
+            <h4 class="price">Investment raised: 1.67cr<br>
+              IRR: 12<br>
+              Investors: 1234<br>
+              On Time Payment: 100%<br>
+              </h4>
+          </div>
+        </div>
+      </div>
+    </section>
+
+           
+    <!-- Testimonials -->
+    <div class="title">
+      <h1>What our Customers say</h1>
+    </div>
+    <div class="owl-carousel owl-theme">
+      <div class="item">
+        <div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            “Grip’s deeply researched instrument offers a great ROI, through a seamless, low friction interface that’s a delight to use and easy to track. It’s excellent post-tax returns offer a unique combination of a yield that’s similar to equity with the risk profile of debt.”
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile1.png" />
+          
+          </div>
+          <h4>Sameer Pitalwalla<br>
+            Founder, CEO at Culture Machine</h4>
+        </div>
+      </div>
+      <div class="item">
+        <div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            Grip has been an amazing partner for us in terms of getting fast access to Capital needed for incurring Capital Expenditures. The team has worked well to create a fast, , reliable and super easy system to ensure that Capital Expenditures does not remain a blocker in the growth of a company.
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile2.png" />
+          </div>
+          <h4>Arun Paliwal<br>
+            Rapidbox</h4>
+        </div>
+      </div>
+      <div class="item"><div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            Participated in Grip’s first furniture investment, providing return of 21%, delivered timely in monthly installments. I was looking for a tailored investment advisory and the team delivered an exemplary customer experience with various options for portfolio diversification.<br>
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile3.png" />
+          </div>
+          <h4>Aditi Gupta<br>
+            Senior Consultant, EY</h4>
+        </div></div>
+      <div class="item">
+        <div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            Sheru is extremely grateful to the entire Grip team for coming up with this amazing platform. Grip unlocked growth capital and serves as a strong marketing channel amongst retail investors. Looking forward to scale the collaboration.
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile3.png" />
+          </div>
+          <h4>Ankit Mittal<br>
+          Sheru</h4>
+        </div>
+      </div>
+      <div class="item">
+        <div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            “Grip solves a very real problem for people who don't have a lot of capital but still want to invest in high-value, high-growth assets. Brilliant concept, awesome team and a very interesting value proposition. All the best!”
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile1.png" />
+          </div>
+          <h4>Tanmay Purohit<br>MBA, ISB Hyderabad</h4>
+        </div>
+      </div>
+      <div class="item">
+        <div class="testimonial">
+          <span>&ldquo;</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nemo expedita doloribus vero facere, quis necessitatibus rem quos deleniti iste ut magnam sapiente reprehenderit placeat alias, quaerat quod adipisci illo.
+          </p>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bx-star"></i>
+          </div>
+          <div class="img-cover">
+            <img class="lazy" data-src="./images/profile2.png" />
+          </div>
+          <h4>aiubi vbv uaivbui</h4>
+        </div>
+      </div> 
+      <div class="testimonial">
+        <span>&ldquo;</span>
+        <p>
+          usabvfui as niubafioio h jd ajdaija Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut reprehenderit quas iure rem consequuntur iusto veniam quaerat omnis nobis perferendis enim velit cum porro eaque, id harum? Adipisci, iure quas!
+        </p>
+        <div class="rating">
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bx-star"></i>
+        </div>
+        <div class="img-cover">
+          <img class="lazy" data-src="./images/profile1.png" />
+        </div>
+        <h4>eiuvsnvnvsou</h4>
+      </div>
+    </div>
+
+<div class="new-img">
+  <img class="lazy" data-src="./images/12.png" style="padding: 5rem;" alt="">
+  <img class="lazy" data-src="./images/13.png" style="padding: 5rem;" alt="">
+  <img class="lazy" data-src="./images/14.png" style="padding: 5rem;" alt="">
+</div>
+
+
+<!--  Brands -->
+    <section class="section">
+      <div class="brands-center container">
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand1.png" alt="" />
+        </div>
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand2.png" alt="" />
+        </div>
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand3.png" alt="" />
+        </div>
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand4.png" alt="" />
+        </div>
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand5.png" alt="" />
+        </div>
+        <div class="brand">
+          <img class="lazy" data-src="./images/brand6.png" alt="" />
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <!-- Footer -->
+  <footer id="footer" class="section footer">
+    <div class="container">
+      <div class="footer-container">
+        <div class="footer-center">
+          <h3>EXTRAS</h3>
+          <a href="#">Partners</a>
+          <a href="#">Refferals</a>
+          <a href="#">Affiliate</a>
+          <a href="#">Specials</a>
+        </div>
+        <div class="footer-center">
+          <h3>INFORMATION</h3>
+          <a href="#">About Us</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Contact Us</a>
+        </div>
+        <div class="footer-center">
+          <h3>MY ACCOUNT</h3>
+          <a href="#">My Account</a>
+          <a href="#">Order History</a>
+          <a href="#">Wish List</a>
+          <a href="#">Newsletter</a>
+          <a href="#">Investments</a>
+        </div>
+        <div class="footer-center">
+          <h3>CONTACT US</h3>
+          <div>
+            <span>
+              <i class="fas fa-map-marker-alt"></i>
+            </span>
+            address full
+          </div>
+          <div>
+            <span>
+              <i class="far fa-envelope"></i>
+            </span>
+            email id 1
+          </div>
+          <div>
+            <span>
+              <i class="fas fa-phone"></i>
+            </span>
+            phone 1
+          </div>
+          <div>
+            <span>
+              <i class="far fa-paper-plane"></i>
+            </span>
+           address line 1
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </footer>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script src="./js/index.js"></script>
+  <script>
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    stagePadding:80,
+    autoplay: true,
+    autoplayTimeout: 6500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+ </script>
+
+ <script>
+  document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;    
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.src = image.dataset.src;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  } else {  
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+ })
+ </script>
+</body>
+
+</html>
